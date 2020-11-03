38d082d2fea995b4aa55a0079a63a2c37450881e
moiz-i-am <moiztahir35@gmail.com>
2020-11-03 12:43:16 +0500

    added reports and configured mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsModule } from './authors/authors.module';
 import { AuthorsController } from './authors/authors.controller';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/AuthorReportsDB'),
     AuthorsModule,
+    ReportsModule,
   ],
   controllers: [AppController, AuthorsController],
   providers: [AppService],
