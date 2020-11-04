fbb36de54fb3fda854a66c42842666dea2d99bf3
moiz-i-am <moiztahir35@gmail.com>
2020-11-04 17:54:24 +0500

    create user with hash pwd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, ValidationPipe } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    const signupCaller = await this.authService.signUp(authCredentialsDto);
+
+    return signupCaller;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+
+import { AuthCredentialsDto } from './auth-credentials.dto';
+import { User } from './user.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new this.userModel({ username, password: hashedPassword });
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('user already exists!!!');
+      }
+      throw error;
+    }
+  }
+}
