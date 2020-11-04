28a31f62aed8b0d9a0e25dc457398c54ef7d5ca9
moiz-i-am <moiztahir35@gmail.com>
2020-11-04 11:28:55 +0500

    add timestamps in schemas

--- src/authors/schemas/authors.schema.ts
+++ src/authors/schemas/authors.schema.ts
@@ -1,5 +1,8 @@
 import * as mongoose from 'mongoose';
 
-export const AuthorSchema = new mongoose.Schema({
-  name: String,
-});
+export const AuthorSchema = new mongoose.Schema(
+  {
+    name: String,
+  },
+  { timestamps: true },
+);
--- src/reports/schemas/reports.schema.ts
+++ src/reports/schemas/reports.schema.ts
@@ -1,7 +1,11 @@
+import { timeStamp } from 'console';
 import * as mongoose from 'mongoose';
 
-export const ReportSchema = new mongoose.Schema({
-  title: String,
-  data: String,
-  authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
-});
+export const ReportSchema = new mongoose.Schema(
+  {
+    title: String,
+    data: String,
+    authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+  },
+  { timestamps: true },
+);
